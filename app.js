2d8115b134a24e76f91821e8185cace21176ed2f
ping58972 <ping58972@gmail.com>
2019-06-07 02:00:47 -0500

    finished login and logout session then store session to mongodb

--- app.js
+++ app.js
@@ -33,15 +33,26 @@ app.use(session({
     store: store
 }));
 
-app.use((req, res, next)=>{
-    User.findById('5cf8915ffcf7093404b2dca4').then(user=> {
-    //User.findById(req.session.user._id).then(user=> {
-        req.user = user;
-       // req.isLoggedIn = true;
-        next();
-    }).catch(err=>console.log(err));
-});
+// app.use((req, res, next)=>{
+//     User.findById('5cf8915ffcf7093404b2dca4').then(user=> {
+//     //User.findById(req.session.user._id).then(user=> {
+//         req.user = user;
+//        // req.isLoggedIn = true;
+//         next();
+//     }).catch(err=>console.log(err));
+// });
 
+app.use((req, res, next) => {
+    if(!req.session.user){
+       return next();
+    }
+    User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err=>console.log(err));
+});
 app.use('/admin', adminRoute);
 app.use(shopRoute);
 app.use(authRoute);
