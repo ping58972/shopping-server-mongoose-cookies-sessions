f88d547ce822b49ab45d11d2c773c7cdc349c71a
ping58972 <ping58972@gmail.com>
2019-06-07 01:09:33 -0500

    fixing login and logout session then store session to mongodb

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
+const User = require('../modles/user');
+
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get('cookie').split(';')[0].trim().split('=')[1] === 'true';
-  console.log(req.session.isLoggedIn);
+  //console.log(req.session.isLoggedIn);
          res.render('auth/login', {
            path: '/login',
            pageTitle: 'Login',
@@ -12,9 +14,19 @@ exports.getLogin = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
     //req.isLoggedIn = true;
     //res.setHeader('Set-Cookie', 'loggedId=true');
-    req.session.isLoggedIn = true;
-         res.redirect('/');
-       
-   
+    User.findById('5cf8915ffcf7093404b2dca4')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/'); 
+    })
+    .catch(err=>console.log(err));
    };
+  
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
   
